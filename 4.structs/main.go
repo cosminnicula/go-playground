0eae6e1a3d941c2eb8b46516cd1eee76c10749bf
Cosmin Nicula <cnicula@axway.com>
2022-05-18 20:02:22 +0300

    Value types, reference types (pointers, pointer shortcuts)

--- 4.structs/main.go
+++ 4.structs/main.go
@@ -50,12 +50,38 @@ func main() {
 		},
 	}
 
+	// pass by value
 	person1.updateName("Johnny")
+	person1.print() // prints John, because receiver parameters are passed as values
+
+	// pass by reference via pointer
+	person1Pointer := &person1
+	person1Pointer.updateNameByReference("Johnny")
 	person1.print()
+
+	// pointer shortcut
+
+	// although  updateNameByReference expects the receiver to be of type pointer
+	// to person, Go automatically coverts the person1 variable of type person into
+	// a pointer to person
+	person1.updateNameByReference("John")
+	person1.print()
+
+	//however, there are both reference and value types:
+	//    - structs are value types
+	//    - slices are structs that contain 3 fields: lenght, capacity, and a pointer to the head of the array.
+	//		  when slices are passed as arguments to a function, the entire (length, capacity, pointer to head) structure
+	//      is copied over, which means that will point to the same array in memory.
+	//value types: int, float, string, bool, struct
+	//reference types: slices, maps, channels, pointers, functions
 }
 
 func (p person) updateName(newFirstName string) {
 	p.firstName = newFirstName
+}
+
+func (pointerToPerson *person) updateNameByReference(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 // print function can be called on any variable of type person
